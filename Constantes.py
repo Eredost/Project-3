e5df876eeb6b1efbfe7e19e74a0345e0f6700a15
Eredost <michael.coutin62@gmail.com>
2018-01-11 10:24:55 +0100

    Adding variables relative to the project

---
+++ Constantes.py
@@ -0,0 +1,19 @@
+#-*-coding:UTF-8 -*-
+
+#################  WINDOWS VARIABLES  #################
+
+sprite_number = 15
+sprite_size = 40
+window_size = sprite_number * sprite_size
+
+window_title = "McGyver in jail"
+window_icon = "mcgyver.png"
+
+
+
+#################  GAMES VARIABLES  #################
+
+
+wall_pic = "wall.png"
+ground_pic = "image1.png"
+guardian_pic ="murdoc.png"
