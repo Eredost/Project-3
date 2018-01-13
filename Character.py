45a5fdc803bfd3f0e7be01fab0f2d64cbc129a7d
Eredost <michael.coutin62@gmail.com>
2018-01-13 14:08:54 +0100

    Adding Character class to project

---
+++ Character.py
@@ -0,0 +1,66 @@
+#-*-coding:UTF-8 -*-
+"""This module create a playable character
+with different mechanics, like move and check
+his position on interface"""
+
+import pygame
+from pygame.locals import *
+
+from Constantes import *
+from Level import *
+
+
+class Character:
+    """This class create the character and setup
+    all his mechanics"""
+
+    def __init__(self, character, level):
+        """Loading sprite of the character and
+        and we implant it as its initial position"""
+
+        self.character = pygame.image.load(character).convert_alpha()
+        self.case_x = 3
+        self.case_y = 13
+        self.pos_x = 120
+        self.pos_y = 520
+        self.level = level
+
+    def move(self, direction):
+        """The x and y position of the character
+        is adjusted according to the player's choice"""
+
+        if direction == "right":
+
+            if self.case_x < (sprite_number - 1):
+
+                if self.level.structure[self.case_y][self.case_x+1] not in "#X/":
+
+                    self.case_x += 1
+                    self.pos_x = self.case_x * sprite_size
+
+        if direction == "left":
+
+            if self.case_x > 0:
+
+                if self.level.structure[self.case_y][self.case_x-1] not in "#X/":
+
+                    self.case_x -= 1
+                    self.pos_x = self.case_x * sprite_size
+
+        if direction == "up":
+
+            if self.case_y > 0:
+
+                if self.level.structure[self.case_y-1][self.case_x] not in "#X/":
+
+                    self.case_y -= 1
+                    self.pos_y = self.case_y * sprite_size
+
+        if direction == "down":
+
+            if self.case_y < (sprite_number - 1):
+
+                if self.level.structure[self.case_y+1][self.case_x] not in "#X/":
+
+                    self.case_y += 1
+                    self.pos_y = self.case_y * sprite_size
