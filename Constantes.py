7e52721ff6040d2e4b588f5e314f61b86046e908
Eredost <michael.coutin62@gmail.com>
2018-01-13 14:07:21 +0100

    Changing sprite number to 17 and deleting comments

--- Constantes.py
+++ Constantes.py
@@ -2,12 +2,12 @@
 
 #################  WINDOWS VARIABLES  #################
 
-sprite_number = 15
+sprite_number = 17
 sprite_size = 40
 window_size = sprite_number * sprite_size
 
 window_title = "McGyver in jail"
-#window_icon = "mcgyver.png"
+window_icon = "Images/mcgyver.png"
 
 
 
@@ -16,4 +16,4 @@ window_title = "McGyver in jail"
 
 wall_pic = "Images/wall.png"
 ground_pic = "Images/image1.png"
-#guardian_pic ="murdoc.png"
+guardian_pic ="Images/murdoc.png"
