259c11ae0eb823e1b02691a15a13fb2dc24876f7
Eredost <michael.coutin62@gmail.com>
2018-01-15 19:24:45 +0100

    Adding object pickup to an inventory

--- Item.py
+++ Item.py
@@ -13,11 +13,14 @@ from Level import *
 
 class Item:
 
-	def __init__(self, item1, item2, item3, level):
+	def __init__(self, item1, item2, item3, level, character):
 		
 		self.items = [item1, item2, item3]
 		self.level = level
+		self.character = character
 		self.items_pos = []
+		self.items_position = []
+		self.inventory = []
 
 		for item in self.items:
 			
@@ -28,8 +31,19 @@ class Item:
 		value = 0
 		
 		for item in self.items:
+
+			if item not in self.inventory:
 			
-			window.blit(item, (self.level.blank_cases[self.items_pos[value]][0],\
-							   self.level.blank_cases[self.items_pos[value]][1]))
+				window.blit(item, (self.level.blank_cases[self.items_pos[value]][0],\
+							   	   self.level.blank_cases[self.items_pos[value]][1]))
+				
+				self.items_position.append([item, self.level.blank_cases[self.items_pos[value]][0], \
+							   	   self.level.blank_cases[self.items_pos[value]][1]])
+
+				if self.character.pos_x == self.level.blank_cases[self.items_pos[value]][0] and \
+				   self.character.pos_y == self.level.blank_cases[self.items_pos[value]][1]:
+
+					self.inventory.append(self.items_position[value][0])
+			
 			value += 1
 
