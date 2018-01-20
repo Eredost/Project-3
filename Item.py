71b22dd130a513b581d21a06015b6ad097da1173
Eredost <michael.coutin62@gmail.com>
2018-01-20 17:18:47 +0100

    Adding docstrings

--- Item.py
+++ Item.py
@@ -4,44 +4,51 @@ the window and check the player position"""
 
 import random
 
-import pygame
-from pygame.locals import *
-
-from Constantes import *
-from Level import *
-
-
 class Item:
+    """This class initialize a random place
+    for items and initialize it on pygame
+    window, create a inventory and fills it
+    according to the position of the character"""
 
-	def __init__(self, item1, item2, item3, level, character):
-		
-		self.items = [item1, item2, item3]
-		self.level = level
-		self.character = character
-		self.items_pos = []
-		self.inventory = []
+    def __init__(self, item1, item2, item3, level, character):
+        """Takes items pictures, the level where they
+        will be incremented and the character to recover
+        his position"""
 
-		for item in self.items:
-			
-			self.items_pos.append(random.randint(0, len(self.level.blank_cases)))
+        self.items = [item1, item2, item3]
+        self.level = level
+        self.character = character
+        self.items_pos = []
+        self.inventory = []
 
-	def placement(self, window):
+        #INIT RANDOM NUMBERS AND ADD IT IN A LIST TO PLACE ITEMS
+        for item in self.items:
 
-		value = 0
-		
-		for item in self.items:
+            self.items_pos.append(random.randint(0, len(self.level.blank_cases)))
 
-			blank_case_x = self.level.blank_cases[self.items_pos[value]][0]
-			blank_case_y = self.level.blank_cases[self.items_pos[value]][1]
+    def placement(self, window):
+        """We get the list of blank cases of the level and with
+        the random numbers, we place them on the game window.
+        We check the player position and if it is on the place
+        of an object, it is added in his inventory and no longer
+        display on screen"""
 
-			if item not in self.inventory:
-			
-				window.blit(item, (blank_case_x, blank_case_y))
+        value = 0
 
-				if self.character.pos_x == blank_case_x and \
-				   self.character.pos_y == blank_case_y:
+        for item in self.items:
 
-					self.inventory.append(item)
-			
-			value += 1
+            #WE SAVE THE POSITION X AND Y OF BLANK CASES IN VARIABLES
+            blank_case_x = self.level.blank_cases[self.items_pos[value]][0]
+            blank_case_y = self.level.blank_cases[self.items_pos[value]][1]
 
+            if item not in self.inventory:
+
+                window.blit(item, (blank_case_x, blank_case_y))
+
+                #IF PLAYER POS IS SAME THAN ITEM POSITION
+                if self.character.pos_x == blank_case_x and \
+                   self.character.pos_y == blank_case_y:
+
+                    self.inventory.append(item)
+
+            value += 1
